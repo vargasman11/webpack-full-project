a7536d902148f5a2af27b0a7ea3568a5a50f77b0
Jerry Vargas <jvargas@conversionia.com>
2021-12-06 13:51:54 -0600

    add

--- webpack.config.js
+++ webpack.config.js
@@ -1,14 +1,23 @@
+const MiniCssExractPlugin = require("mini-css-extract-plugin");
 const path = require('path');
+
 let mode = "development";
+let target = "web";
 
 if (process.env.NODE_ENV === "production"){
     mode = "production";
+    target = "browserslist";
 }
 
 module.exports = {
     mode:mode,
+    target: target,
     module: {
         rules: [
+            {
+                test: /\.s?css$/i,
+                use:[MiniCssExractPlugin.loader, 'css-loader', "postcss-loader", "sass-loader"]
+            },
             {
                 test: /\.js$/,
                 exclude: /node_modules/,
@@ -18,10 +27,12 @@ module.exports = {
             }
         ]
     },
+    plugins: [ new MiniCssExractPlugin()],
     devtool: "source-map",
     devServer: {
         static:{
-            directory:path.join(__dirname, 'dist')
-        }
+            directory:path.join(__dirname, 'dist'),
+        },
+        hot:true
     },
 }
